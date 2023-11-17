b46ddf721ceaa7b5230d75d85b865863bab0bc4f
Aditya Kumar <116816791+imadityakr@users.noreply.github.com>
2023-11-17 09:05:17 +0530

    update

--- service-worker.js
+++ service-worker.js
@@ -2,14 +2,12 @@ const CACHE_NAME = 'my-pwa-cache-v1';
 const urlsToCache = [
   '/', 'index.html', 'style.css', 'app.js'
 ];
-
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(urlsToCache))
   );
 });
-
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
